659fa59a6491c229d7517511451c248714b52aab
Andrew Tsvelodub <andts1000@gmail.com>
2018-06-10 11:11:18 +0300

    path changing interview task in rust
     * skip empty path elements
     * add explicit root element

--- path-changer/src/main.rs
+++ path-changer/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     let target_path: Vec<&str> = args[1].split("/").collect();
     let curr_path_norm = calculate_path(&Vec::new(), &current_path);
     let target_path_result = calculate_path(&curr_path_norm, &target_path);
-    let result_path_str = target_path_result.join("/");
-
-    println!("result path: {:?}", result_path_str);
+    let mut result = String::from("/");
+    result.push_str(&target_path_result.join("/"));
+    println!("result path: {:?}", result);
 }
 
 fn calculate_path<'a>(curr_path: &[&'a str], target_path: &[&'a str]) -> Vec<&'a str> {
@@ -20,7 +20,9 @@ fn calculate_path<'a>(curr_path: &[&'a str], target_path: &[&'a str]) -> Vec<&'a
             }
             result_path.pop();
         } else {
-            result_path.push(*s);
+            if !s.is_empty() {
+                result_path.push(*s);
+            }
         }
     }
     result_path
